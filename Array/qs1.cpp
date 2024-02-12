35b9668469b392c50c7400313bb53c2cbd4861f2
Sachin Aswal <91174800+Sachinaswal29@users.noreply.github.com>
2024-02-12 12:48:38 +0530

    Add files via upload

---
+++ Array/qs1.cpp
@@ -0,0 +1,18 @@
+// Reverse the array
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int *a=new int[n];
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    int i = 0,j = n - 1;
+    while (i < j) swap(a[i++], a[j--]);
+    for (int i = 0; i < n; i++){
+        cout << a[i] << " ";
+    }
+    return 0;
+}
